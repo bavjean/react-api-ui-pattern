41316dcbde2dc9acfefdc864a801235f3a39463d
Brian Avjean <bavjean@gmail.com>
2020-05-22 09:03:42 -0400

    changed title

--- src/Accordion.js
+++ src/Accordion.js
@@ -3,7 +3,7 @@ import AccordionContent from "./AccordionContent";
 import "./Accordion.css";
 
 function Accordion(props) {
-  // console.log(props)
+//   console.log(props)
   return (
     <div>
       <button onClick={props.toggle}>{props.brewery.name}</button>
